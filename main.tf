019d62cb20db68caa4736b0e3787dfb5722545e4
ANPATIL\arunp <arun>
2024-02-07 15:13:49 +0530

    added

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"  # specify the AMI ID
+  ami           = "ami-08e2c1a8d17c2fe17"  # specify the AMI ID
   instance_type = "t2.micro"  # specify the instance type
 }

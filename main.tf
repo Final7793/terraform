cf884e8c7ad04843b95ebca5b1210920430699b2
ANPATIL\arunp <arun>
2024-02-16 17:09:33 +0530

    Added

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
 resource "aws_instance" "example" {
   ami           = "ami-01e82af4e524a0aa3"  # Specify the AMI ID for your EC2 instance
-  instance_type = "t2.micro"                # Specify the instance type (e.g., t2.micro, t3.small)
+  instance_type = "var.instance_type"                # Specify the instance type (e.g., t2.micro, t3.small)
 
   tags = {
     Name = "example-instance"  # Specify a name for your instance
